d152e0707865c7e6e33ce02fd7b97fc08b320ce7
Todd Eidson <todd@eidson.info>
2021-01-26 07:24:15 -0500

    Init commit

---
+++ src/main/java/info/eidson/hello/Hello.java
@@ -0,0 +1,7 @@
+package info.eidson.hello;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins");
+    }
+}
---
+++ src/test/java/info/eidson/hello/HelloTest.java
@@ -0,0 +1,13 @@
+package info.eidson.hello;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+}
