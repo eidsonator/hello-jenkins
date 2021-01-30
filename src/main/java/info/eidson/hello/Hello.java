851d96e8c9ee47943c830ef343f549a268b471f0
Todd Eidson <todd@eidson.info>
2021-01-30 11:42:00 -0500

    quality gate

--- src/main/java/info/eidson/hello/Hello.java
+++ src/main/java/info/eidson/hello/Hello.java
@@ -4,4 +4,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Jenkins");
     }
+
+    public void what() {
+        System.out.println("Tautology");
+    }
 }
--- src/test/java/info/eidson/hello/HelloTest.java
+++ src/test/java/info/eidson/hello/HelloTest.java
@@ -1,18 +1,45 @@
 package info.eidson.hello;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
 
-    @Test
-    void main() {
-        assertTrue(true);
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
     }
 
     @Test
-    void test() {
-        assertTrue(true);
+    public void out() {
+        Hello.main(null);
+        assertEquals("Hello Jenkins\n", outContent.toString());
     }
+
+    @Test
+    public void test_what() {
+        Hello hello = new Hello();
+        hello.what();
+        assertEquals("Tautology\n", outContent.toString());
+    }
+
 }
